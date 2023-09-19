90acf1731f23ca3250c4d4242e01ae57b83ed9f8
james <james@irm.kr>
2023-09-19 10:29:03 +0900

    [Feature]: react composite pattern

--- src/App.js
+++ src/App.js
@@ -1,25 +1,98 @@
 import logo from './logo.svg';
 import './App.css';
+import {useEffect, useState} from 'react';
+
+const TextFieldWithLabel = withLabel(withError(TextField));
+
 
 function App() {
+
+    const [value, setValue] = useState(null);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        setTimeout(() => {
+            setError('Error....rrrr ');
+        }, 4000)
+    }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+        {/*<TextField*/}
+        <TextFieldWithLabel
+            // label={'label.....'}
+            value={value}
+            onChange={setValue}
+            placeholder={'please enter.....'}
+            // error={error}
         >
-          Learn React
-        </a>
-      </header>
+            High order Component label
+        </TextFieldWithLabel>
+        <>
+            <LabelAndButton>
+                버튼을 위한 레이블
+            </LabelAndButton>
+        </>
     </div>
   );
+}
+
+function withLabel (Component) {
+
+    return ({children, ...rest}) => {
+        console.log('라벨 컴포넌트  ', children , rest);
+        return (
+        <div>
+            <label className='text-label'>{children}</label>
+            <Component {...rest}/>
+        </div>
+    )}
+}
+
+function withError (Component) {
+    return ({error = 'Error', ...rest}) => {
+        console.log('에러 컴포넌트  ', error, rest)
+        return (
+            <>
+                < Component {...rest} />
+                <div className='error'>{error}</div>
+            </>
+        )
+    }
+}
+
+function TextField({value, onChange, placeholder}) {
+
+    return (
+        <div>
+            <input
+                value={value}
+                type='text'
+                placeholder={placeholder}
+                className={'text-input'}
+                onChange={(e) => onChange(e.target.value)}
+            />
+        </div>
+    )
+}
+
+const LabelAndButton = withLabel(CustomButton);
+
+function CustomButton() {
+
+    const handleSubmitBtn = () => {
+        console.log('전송 버튼 클릭 ');
+    }
+
+    return (
+        <>
+            <button
+                onClick = {handleSubmitBtn}
+            >
+                전송
+            </button>
+        </>
+    )
 }
 
 export default App;
